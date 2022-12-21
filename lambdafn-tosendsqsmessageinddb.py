325ad2d145d5d3ae3a66b4e9bea7ea78cb9eff30
Shubham Yadav <shubham05071998@gmail.com>
2022-12-21 12:35:27 +0530

    Create lambdafn-tosendsqsmessageinddb.py

---
+++ lambdafn-tosendsqsmessageinddb.py
@@ -0,0 +1,29 @@
+#SQS is addee as trigger in lambda
+from datetime import datetime
+import json
+import os
+import boto3
+
+dynamodb = boto3.resource('dynamodb')
+
+def lambda_handler(event, context):
+    # Count items in the Lambda event 
+    print(event)
+    no_messages = str(len(event['Records']))
+    print("Found " +no_messages +" messages to process.")
+
+    for message in event['Records']:
+
+        print(message)
+
+        # Write message to DynamoDB
+        table = dynamodb.Table('Message')
+
+        response = table.put_item(
+            Item={
+                'MessageId': message['messageId'],
+                'Body': message['body'],
+                'Timestamp': datetime.now().isoformat()
+            }
+        )
+        print("Wrote message to DynamoDB:", json.dumps(response))
